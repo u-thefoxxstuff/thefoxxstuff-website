db8fee8608bf9e48091d78effb543c6f0cfddd6b
Aytal Popov <42153584+createandchoose@users.noreply.github.com>
2022-05-18 00:33:39 +0900

    ASD
    ASD

---
+++ thefoxxstuff-Vue/thefoxxstuff/src/components/Furry.vue
@@ -0,0 +1,16 @@
+<template>
+    <li>
+        {{fur.id}}) {{fur.title}}
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        fur: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
---
+++ thefoxxstuff-Vue/thefoxxstuff/src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ul>
+            <TodoItem
+            v-for="(todo, index) in todos" :key="index"
+            v-bind:todo="todo"
+            />
+            <Furry
+            v-for="(fur, indexx) in furry" :key="indexx"
+            v-bind:fur="fur"
+            />
+        </ul>
+        
+    </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem'
+import Furry from '@/components/Furry'
+export default {
+    props: ['todos', 'furry'],
+    components: {
+        TodoItem,
+        Furry,
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: 0;
+    margin: 0;
+    padding: 0;
+}
+</style>
